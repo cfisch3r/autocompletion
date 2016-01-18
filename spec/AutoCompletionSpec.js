133c57e11e42ea1e9cabb750eb78bb7362ed5a10
Christian Fischer <christian.fischer@colenet.de>
2016-01-18 14:36:17 +0100

    refactoring

--- spec/AutoCompletionSpec.js
+++ spec/AutoCompletionSpec.js
@@ -11,28 +11,29 @@ describe("AutoCompletion", function() {
 	// Unit of Work
 	var sut;
 	
+	var suggestions = ['item'];
+	
 	beforeEach(function(){
 		fetchSuggestions = jasmine.createSpy('fetchSuggestions').and.callFake(function(phrase,callback){
-				callback(['item']);				
+				callback(suggestions);				
 		});
 		displaySuggestions = jasmine.createSpy('displaySuggestions');
 		sut = new AutoCompletion(fetchSuggestions,displaySuggestions);
 	});
-/*	
-    it('should display fetched suggestions.', function(){
-		sut.search('phrase');
-		expect(displaySuggestions).toHaveBeenCalledWith(['item']);
-    });
-
+/*
 	it('should fetch Suggestions for search phrase', function(){
 		sut.search('phrase');
 		expect(fetchSuggestions).toHaveBeenCalledWith('phrase');		
 	});
+
+    it('should display fetched suggestions.', function(){
+		sut.search('phrase');
+		expect(displaySuggestions.toHaveBeenCalledWith(suggestions);
+    });
     
 	it('should not fetch Suggestions for search phrase with less then 3 characters', function(){
 		sut.search('ph');
 		expect(fetchSuggestions).not.toHaveBeenCalled();		
 	});
-*/    
-
+*/
 });
